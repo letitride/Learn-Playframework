0c9fb32f984e43fd9b1962af3d1ec69776160bb8
letitride <ichikawa.fumiya@gmail.com>
2020-04-18 21:49:47 +0900

    chapter03 view seleniumテスト

---
+++ test/seleniums/ViewIndexSpec.scala
@@ -0,0 +1,84 @@
+package seleniums
+
+import org.openqa.selenium.WebDriver
+import org.scalatestplus.play.guice.GuiceOneServerPerTest
+import org.scalatestplus.play.{HtmlUnitFactory, OneBrowserPerSuite, PlaySpec}
+
+class IndexSpec extends PlaySpec with GuiceOneServerPerTest with OneBrowserPerSuite with HtmlUnitFactory{
+
+  import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
+  override def createWebDriver(): WebDriver = {
+    val driver = new HtmlUnitDriver {
+      def setAcceptLanguage(lang: String) =
+        this.getWebClient().addRequestHeader("Accept-Language", lang)
+    }
+    driver.setAcceptLanguage("en")
+    driver
+  }
+
+  "GET /" should {
+    "view表示のテスト" in {
+      go to s"http://localhost:$port/"
+      assert(pageTitle === "index view")
+      assert(findAll(className("post-body")).length === 0)
+    }
+  }
+/*
+  "POST /" should {
+    "投稿したものが表示される" in {
+      val body = "test post"
+
+      go to s"http://localhost:$port/"
+      textField(cssSelector("input#post")).value = body
+      submit()
+
+      eventually {
+        val posts = findAll(className("post-body")).toSeq
+        assert(posts.length === 1)
+        assert(posts(0).text === body)
+        assert(findAll(cssSelector("p#error")).length === 0)
+      }
+    }
+  }
+
+  "POST /" should {
+    "投稿したものが表示される その2" in {
+      go to s"http://localhost:$port/"
+
+      eventually {
+        val posts = findAll(className("post-body")).toSeq
+        assert(posts.length === 1)
+      }
+    }
+
+    "空のメッセージは投稿できない" in {
+      val body = ""
+
+      go to s"http://localhost:$port/"
+      textField(cssSelector("input#post")).value = body
+      submit()
+
+      eventually {
+        val error = findAll(cssSelector("p#error")).toSeq
+        assert(error.length === 1)
+        assert(error(0).text === "Please enter a message.")
+      }
+    }
+
+    "長すぎるメッセージは投稿できない" in {
+      val body = "too long messages"
+
+      go to s"http://localhost:$port/"
+      textField(cssSelector("input#post")).value = body
+      submit()
+
+      eventually {
+        val error = findAll(cssSelector("p#error")).toSeq
+        assert(error.length === 1)
+        assert(error(0).text === "The message is too long.")
+      }
+    }
+  }
+ */
+}
