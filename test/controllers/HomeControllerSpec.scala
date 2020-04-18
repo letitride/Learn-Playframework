0ceed3f93f77cecff68ea271f41166a2f9f6f72b
letitride <ichikawa.fumiya@gmail.com>
2020-04-18 21:18:05 +0900

    chapter02 session controllerテスト

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -18,37 +18,32 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
     "render the index page from a new instance of controller" in {
       val controller = new HomeController(stubControllerComponents())
-      val home = controller.index(Some("my")).apply(FakeRequest(GET, "/"))
+      val home = controller.index(Some("my"), Some("you")).apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      println( cookies(home).get("name").getOrElse(Cookie("name", "no-send")).value )
-      cookies(home).get("name").getOrElse(Cookie("name", "no-send")).value mustBe "my"
-      contentAsString(home) must include ("nameがおくられました")
+      session(home).get("my").getOrElse("") mustBe "you"
     }
 
-    "cookie送信時の表示のテスト" in {
+    "session送信時の表示のテスト" in {
       val controller = inject[HomeController]
-      val home = controller.index(None).apply(FakeRequest(GET, "/").withCookies(Cookie("name", "cookiedata")) )
-      contentAsString(home) must include ("cookiedata")
+      val home = controller.index(None, None).apply(FakeRequest(GET, "/")
+        .withSession("session-key" -> "session1"))
+      contentAsString(home) must include ("session1")
     }
 
     "パラメータなしでindexメソッドを実行のテスト" in {
       val controller = inject[HomeController]
-      val home = controller.index(None).apply(FakeRequest(GET, "/"))
+      val home = controller.index(None, None).apply(FakeRequest(GET, "/"))
       status(home) mustBe OK
-      //cookies(home).get("name").get mustBe None
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("nameはありません")
     }
 
     "render the index page from the application" in {
       val controller = inject[HomeController]
-      val home = controller.index(Some("you")).apply(FakeRequest(GET, "/"))
-
+      val home = controller.index(Some("you"), None).apply(FakeRequest(GET, "/"))
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Hello")
     }
 
     "render the index page from the router" in {
@@ -57,7 +52,6 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Hello")
     }
   }
 }
